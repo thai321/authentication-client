0db2f72a91cd141dcacc2a56b965a50959399017
Quoc Thai Nguyen Truong <thai.nguyen@berkeley.edu>
2017-06-28 23:52:16 -0700

    install redux-thunk

--- src/components/auth/signin.js
+++ src/components/auth/signin.js
@@ -1,15 +1,9 @@
 import React, { Component } from 'react';
-import { reduxForm, Field } from 'redux-form';
+import { reduxForm } from 'redux-form';
 
 class Signin extends Component {
-  constructor(props) {
-    super(props);
-
-    this.handleFormSubmit = this.handleFormSubmit.bind(this);
-  }
 
   handleFormSubmit({ email, password }) {
-    console.log("this.props = ", this.props.email);
     console.log(email, password);
     // Need to do something to log user in
   }
@@ -17,21 +11,20 @@ class Signin extends Component {
   render() {
     // from reduxForm
     const { handleSubmit, fields: { email, password }} = this.props;
-    // console.log("this.props = ", this.props);
 
     return (
       <form
-        onSubmit={handleSubmit(this.handleFormSubmit)}
+        onSubmit={handleSubmit(this.handleFormSubmit.bind(this))}
         >
 
         <fieldset className="form-group" >
           <label>Email:</label>
-          <Field {...email} className="form-control" />
+          <input {...email} className="form-control" />
         </fieldset>
 
         <fieldset className="form-group" >
           <label>Password:</label>
-          <Field {...password} className="form-control" />
+          <input {...password} className="form-control" />
         </fieldset>
 
         <button action="submit" className="btn btn-primary">Sign In</button>
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,8 +1,8 @@
 import { combineReducers } from 'redux';
-import { reducer } from 'redux-form';
+import { reducer as form} from 'redux-form';
 
 const rootReducer = combineReducers({
-  form: reducer
+  form
 });
 
 export default rootReducer;
